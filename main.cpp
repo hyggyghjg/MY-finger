e93c2b508f436c1a687ac94d24e587c36b3e6a0f
hyggyghjg <1298518582@qq.com>
2021-11-24 20:11:01 +0800

    first

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#define ElemType char
+class List
+{
+    ElemType elem;
+    List *next;
+};
+List HeadInset()
+{
+    List head =
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
